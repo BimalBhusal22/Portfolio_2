01616cde5971431a9a6fe706ac3a252185a81d71
BimalBhusal22 <bimalbhusal1st2075@gmail.com>
2025-08-30 11:48:31 +0545

    fix responsiveness issue in NumberGame component

--- src/components/NumberGame.jsx
+++ src/components/NumberGame.jsx
@@ -4,38 +4,55 @@ const NumberGame = () => {
 
 
       <div className="h-[45vw]  basis-[100%] sm:basis-1/2 lg:basis-1/3 flex flex-column flex-wrap justify-center  content-center">
-        <div className=" font-['Monoton'] text-[#640C19] text-[6.4vw] relative flex justify-center uppercase ">
+      <div className="basis-[100%]">
+      <div className="flex justify-center">
+      <div className="">
+        <div className=" font-['Monoton'] text-[#640C19] text-[6.4vw] uppercase ">
           80+
-          <div className="text-[calc(8px+0.5vw)] w-[100%] absolute bottom-0">
+          </div>
+          <div className="  text-[calc(8px+0.5vw)] font-['Monoton'] uppercase">
             projects
           </div>
         </div>
+        </div>
+        </div>
         <img src="80plusProjects.png" className="h-[18vw] " />
       </div>
 
 
       <div className="h-[45vw]  basis-[100%] sm:basis-1/2 lg:basis-1/3 flex flex-column flex-wrap justify-center  content-center">
-      <div className="w-[100%] flex justify-center">
-        <img src="10plusCertificates.png" className="h-[18vw] " />
-        </div>
-        <div className=" font-['Monoton'] text-[#640C19] text-[6.4vw] relative flex justify-center uppercase ">
+      <img src="10plusCertificates.png" className="h-[18vw] " />
+      <div className="basis-[100%]">
+      <div className="flex justify-center">
+      <div className="">
+        <div className=" font-['Monoton'] text-[#640C19] text-[6.4vw] uppercase ">
           10+
-          <div className="text-[calc(8px+0.5vw)] w-[100%] absolute bottom-0">
+          </div>
+          <div className="  text-[calc(8px+0.5vw)] font-['Monoton'] uppercase">
             certificates
           </div>
         </div>
+        </div>
+        </div>
+        
       </div>
 
-
       <div className="h-[45vw]  basis-[100%] sm:basis-1/2 lg:basis-1/3 flex flex-column flex-wrap justify-center  content-center">
-        <div className=" font-['Monoton'] text-[#640C19] text-[6.4vw] relative flex justify-center uppercase ">
+      <div className="basis-[100%]">
+      <div className="flex justify-center">
+      <div className="">
+        <div className=" font-['Monoton'] text-[#640C19] text-[6.4vw] uppercase ">
           100+
-          <div className="text-[calc(8px+0.5vw)] w-[100%] absolute bottom-0">
+          </div>
+          <div className="  text-[calc(8px+0.5vw)] font-['Monoton'] uppercase">
             happy clients
           </div>
         </div>
+        </div>
+        </div>
         <img src="80plusProjects.png" className="h-[18vw] " />
       </div>
+
     </div>
   );
 };
